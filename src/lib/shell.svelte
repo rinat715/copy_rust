e3206af3c3ac3b31e46b0ff8f639dac38a6c5c5b
rinat715 <azat715@gmail.com>
2023-01-27 14:47:18 +0500

    вариант

---
+++ src/lib/shell.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { fns, AppShell, Navbar, } from '@svelteuidev/core';
+	import NavContent from './NavContent.svelte';
+
+	let isDark = false;
+	let opened = false;
+
+	function toggleTheme() {
+		isDark = !isDark;
+	}
+	function toggleOpened() {
+		opened = !opened;
+	}
+</script>
+
+<AppShell>
+	<Navbar slot="navbar" hidden={!opened}
+	width={{
+        // When viewport is larger than theme.breakpoints.sm, Navbar width will be 300px
+        sm: 300,
+
+        // When viewport is larger than theme.breakpoints.lg, Navbar width will be 400px
+        lg: 400,
+
+        // When other breakpoints do not match base width is used, defaults to 100%
+        base: 100
+    }}>
+		
+		<NavContent 
+		/>
+	</Navbar>
+
+	<slot>This is the main content</slot>
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Greet from "$lib/Greet.svelte";
+  import Shell from "$lib/shell.svelte";
+</script>
+
+<h1>Welcome to Tauri!</h1>
+
+<Shell />
